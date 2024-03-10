c6d1b0b08a7c2f4e42174b65aa99a7466db877de
Dikesh3637 <dikeshreddy.dev@gmail.com>
2024-03-10 16:50:26 +0530

    update server endpoint for socket io

--- src/client-socket.ts
+++ src/client-socket.ts
@@ -1,10 +1,14 @@
 import { io } from "socket.io-client";
 
 // Replace "http://localhost:3000" with the actual URL where your socket.io server is running
-const socket = io("http://localhost:3000");
+const socket = io("https://mind-sprint-server.onrender.com");
 
-export const adminNamespace = io("http://localhost:3000/admin-dash");
+export const adminNamespace = io(
+  "https://mind-sprint-server.onrender.com/admin-dash"
+);
 
-export const answerNamespace = io("http://localhost:3000/answer");
+export const answerNamespace = io(
+  "https://mind-sprint-server.onrender.com/answer"
+);
 
 export default socket;
